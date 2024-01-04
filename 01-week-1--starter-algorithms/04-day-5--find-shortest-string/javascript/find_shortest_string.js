3bb432eb1113410de0346ef6bcbaafcdcf8411b6
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-01-03 20:09:08 -0800

    Completes lab

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,21 +1,33 @@
 function findShortestString(arr) {
-  // type your code here
+  let stringLength = arr[0].length;
+  let stringIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let currentLength = arr[i].length;
+    if (stringLength > currentLength) {
+      stringIndex = i;
+      break;
+    }
+  }
+  return arr[stringIndex];
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestString(["aaa", "a", "bb", "ccc"]));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestString(["cat", "hi", "dog", "an"]));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log(
+    "=>",
+    findShortestString(["flower", "juniper", "lily", "dadelion"])
+  );
 
   // BENCHMARK HERE
 }
