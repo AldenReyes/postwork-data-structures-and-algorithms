84fde46d8211cf184057700ebc2789b32992b44f
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-01-16 20:12:35 -0800

    Completes lab

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -1,21 +1,45 @@
+/**
+ *For every character of string, if character === '(', add to open.
+ *If character === ')', subtract from open as long as open is > 0, else
+ *add to missing. Return sum of open and missing.
+ */
 function balancingParentheses(string) {
-  // type your code here
+  let open = 0;
+  let missing = 0;
+  for (const char of string) {
+    if (char === '(') {
+      open++;
+      continue;
+    } else if (char === ')') {
+      if (open > 0) {
+        open--;
+      } else {
+        missing++;
+      }
+    }
+  }
+  return open + missing;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 0");
+  console.log('Expecting: 0');
   console.log(balancingParentheses('(()())'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: 2");
+  console.log('Expecting: 2');
   console.log(balancingParentheses('()))'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: 1");
+  console.log('Expecting: 1');
   console.log(balancingParentheses(')'));
+
+  console.log('');
+
+  console.log('Expecting: 2');
+  console.log(balancingParentheses(')('));
 }
 
 module.exports = balancingParentheses;
