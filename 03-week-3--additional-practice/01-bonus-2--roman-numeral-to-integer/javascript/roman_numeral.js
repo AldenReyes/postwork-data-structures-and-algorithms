085ccdecf54f7df3d9041d74ebe942d7c727e806
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-01-18 03:39:07 -0800

    Completes lab

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,21 +1,51 @@
+/**
+ * Converts a string roman numeral into its number equivalent.
+ * Determines numeral by comparing the values of adjacent characters
+ * Example: VI = 6 vs IV = 4
+ */
 function romanNumeral(string) {
-  // type your code here
+  const romanChars = string.split('');
+  const romanObj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
+  for (let i = 0; i < romanChars.length; i++) {
+    const current = romanObj[romanChars[i]];
+    const next = romanObj[romanChars[i + 1]];
+    if (next !== undefined && current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+  return total;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 1");
+  console.log('Expecting: 1');
   console.log(romanNumeral('I'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: 9");
+  console.log('Expecting: 9');
   console.log(romanNumeral('IX'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: 402");
+  console.log('Expecting: 402');
   console.log(romanNumeral('CDII'));
+
+  console.log('');
+
+  console.log('Expecting: 3799');
+  console.log(romanNumeral('MMMDCCXCIX'));
 }
 
 module.exports = romanNumeral;
