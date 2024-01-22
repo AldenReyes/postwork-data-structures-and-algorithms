5e15a63ca3cff071dc0fc1b9c29be7ad5c13ecf7
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-01-22 08:30:45 -0800

    Completes lab

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,21 +1,26 @@
 function rotateArray(arr, k) {
-  // type your code here
+  /**Rotates array by k units by removing k units from the input array
+   * then concatenating the removed elements with the remaining array.
+   **/
+  const rotations = k % arr.length;
+  const removed = arr.splice(-rotations);
+  return removed.concat(arr);
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [4, 1, 2, 3]");
-  console.log("=>", rotateArray([1, 2, 3, 4], 1));
+  console.log('Expecting: [4, 1, 2, 3]');
+  console.log('=>', rotateArray([1, 2, 3, 4], 1));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: [2, 3, 1]");
-  console.log("=>", rotateArray([1, 2, 3], 2));
+  console.log('Expecting: [2, 3, 1]');
+  console.log('=>', rotateArray([1, 2, 3], 2));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: [1, 2, 3]");
-  console.log("=>", rotateArray([1, 2, 3], 3));
+  console.log('Expecting: [1, 2, 3]');
+  console.log('=>', rotateArray([1, 2, 3], 6));
 }
 
 module.exports = rotateArray;
