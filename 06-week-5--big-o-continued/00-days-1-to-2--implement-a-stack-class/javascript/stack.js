ced11cb026a19d26a9f6866511708354810d8ec5
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-02-02 21:15:52 -0800

    Completes lab

--- 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/javascript/stack.js
+++ 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/javascript/stack.js
@@ -9,43 +9,60 @@ class Stack {
   // add item to top of stack if not full
   // if full throw error
   push(item) {
-
+    if (!this.isFull()) {
+      this.stack.push(item);
+    } else {
+      throw new Error('Stack is full');
+    }
   }
 
   // remove item from top of stack and return it
   pop() {
-
+    if (!this.isEmpty()) {
+      return this.stack.pop();
+    }
   }
 
   // return item at top of stack without removing it
   peek() {
-
+    if (!this.isEmpty()) {
+      return this.stack[this.stack.length - 1];
+    }
   }
 
   // return true if stack is empty, otherwise false
   isEmpty() {
-
+    return this.stack.length === 0;
   }
 
   // return true if stack is full, otherwise false
   isFull() {
-
+    return this.stack.length === this.limit;
   }
 
   // return number of items in stack
   size() {
-
+    return this.stack.length;
   }
 
-  // return -1 if item not in stack, otherwise integer representing 
+  // return -1 if item not in stack, otherwise integer representing
   // how far it is from the top
   search(target) {
-
+    let counter = this.stack.length - 1;
+    for (const element of this.stack) {
+      if (element === target) {
+        return counter;
+      }
+      counter--;
+    }
+    return -1;
   }
 
   // print contents of stack: do not return the stack itself!
   print() {
-    
+    this.stack.forEach((element) => {
+      console.log(element);
+    });
   }
 }
 
